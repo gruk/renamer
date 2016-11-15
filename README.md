e4c88dc7a4f161ced9c0427e69fefd1d4ecbf9a1
James <james.mckelvie@gmail.com>
2016-11-16 12:37:02 +1300

    completed first draft

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Renamer for 4k dpx files
+#Renamer for 4k dpx files
+
+Requires ImageMagick and Wand to be installed.
+
+brew install imagemagick
+pip install wand
--- renamer.py
+++ renamer.py
@@ -1,75 +1,70 @@
 #!/usr/bin/env python
-# from __future__ import print_function
-# from wand.image import Image
-# from __future__ import print_function
+
+#IMPORTING MODULES
 import os, re
 from wand.image import Image
 
+#DEFINE GLOBAL VARIABLE
+counter = 1000
 
 def main():
+	#INTRO
 	print "File renumbererer for Tony"
 	print ""
+	#Collect Path
 	path = raw_input("enter folder path: ").strip()
+	#Scan Path
 	scan(path)
 
 def scan(path):
-	results = []
+	#Create list
+
+	#Look through Directory
 	for root, dirs, files in os.walk(path):
+		#Loop through all files
 		for name in files:
+			#If file is hidden - ignore it. Otherwise send it to checker
 			if re.search('^\..*', name) == None:
-				results.append(os.path.join(root, name))
+				# pass both path+name and filename to checker
+				check(os.path.join(root, name), name, root)
 			else:
 				pass
 
 
-	for item in results:
-		# print item
-		checkres(item)
+#Check Resolution of files
+def check(item,name, path):
+	#If file size is less than 15MB
+	if os.stat(item).st_size <= 15000000:
+		#Use ImageMagick to check the dimensions of the image
+		with Image(filename=item) as img:
+			#If image width is less than 2200 pixels - send it to the renaming function
+			if img.width <= 2200:
+				#pass path+name and name to renamer
+				rename(item, name, path)
+			#Otherwise print error
+			else:
+				print "ERROR: File '%s' has a width greater than 2200 pixels" %item
+	#Otherwise print error
+	else:
+		print "ERROR: file '%s' is larger than 15MB" %item
 
-def checkres(item):
-	with Image(filename=item) as img:
-		print item
-		print 'width =', img.width
-		print 'height =', img.height
-
+#Rename function
+def rename(item, name, path):
+	global counter
+	# add 1 to the counter
+	counter += 1
+	#Prove that data gets here
+	print "file '%s' at path '%s' will be renamed to: " %(name, item)
+	#set name variable
+	init_name = name
+	#remove current numbers from the name
+	split_name = init_name.split('.',1)[0]
+	#add new number to the name and add .dpx
+	new_name = split_name + '.' + str(counter) + '.dpx'
+	#print finalised name
+	new_full_name = path + '/' + new_name
+	os.rename(item, new_full_name)
 
 if __name__ == '__main__':
 	main()
-
-
-
-	# def find_all(name, path):
-	# # searches for a file in a path and returns a list of the filepaths
-
-	# strippedName = name.strip().split('.')
-	# cleanName = strippedName[0]
-	# MOVName = cleanName + '.MOV'
-	# movName = cleanName + '.mov'
-	# mp4Name = cleanName + '.mp4'
-	# for root, dirs, files in os.walk(path):
-	# 	if MOVName in files:
-	# 		clipDict2[MOVName] = clipDict[name]
-	# 		searchResult.append(os.path.join(root, MOVName))
-	# 		print "%i files found" % count
-	# 		count += 1
-	# for root, dirs, files in os.walk(path):
-	# 	if movName in files:
-	# 		clipDict2[movName] = clipDict[name]
-	# 		searchResult.append(os.path.join(root, movName))
-	# 		print "%i files found" % count
-	# 		count += 1
-	# for root, dirs, files in os.walk(path):
-	# 	if cleanName in files:
-	# 		clipDict2[cleanName] = clipDict[name]
-	# 		searchResult.append(os.path.join(root, cleanName))
-	# 		print "%i files found" % count
-	# 		count += 1
-	# for root, dirs, files in os.walk(path):
-	# 	if mp4Name in files:
-	# 		clipDict2[mp4Name] = clipDict[name]
-	# 		searchResult.append(os.path.join(root, mp4Name))
-	# 		print "%i files found" % count
-	# 		count += 1
-	# return searchResult
-
 
